325b61a443108536430369b7f970f50f33873e06
John Blaiklock <miniwinwm@gmail.com>
2018-02-04 09:04:59 +0000

    3d controls

---
+++ MiniWin/bitmaps/tick.c
@@ -0,0 +1,84 @@
+/*
+
+MIT License
+
+Copyright (c) John Blaiklock 2018 miniwin Embedded Window Manager
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/***************
+*** INCLUDES ***
+***************/
+
+#include <stdint.h>
+
+/****************
+*** CONSTANTS ***
+****************/
+
+/* Bitmap width: 12 */
+/* Bitmap height: 12 */
+
+const uint8_t mw_bitmaps_tick[] = {
+  0xff, 0xf0,
+  0xff, 0x90,
+  0xff, 0x30,
+  0xfe, 0x70,
+  0xfc, 0xf0,
+  0xf8, 0xf0,
+  0xf9, 0xf0,
+  0x31, 0xf0,
+  0x83, 0xf0,
+  0xc3, 0xf0,
+  0xe7, 0xf0,
+  0xf7, 0xf0};
+
+/************
+*** TYPES ***
+************/
+
+/***********************
+*** GLOBAL VARIABLES ***
+***********************/
+
+/***********************
+*** GLOBAL VARIABLES ***
+***********************/
+
+/*************************
+*** EXTERNAL VARIABLES ***
+*************************/
+
+/**********************
+*** LOCAL VARIABLES ***
+**********************/
+
+/********************************
+*** LOCAL FUNCTION PROTOTYPES ***
+********************************/
+
+/**********************
+*** LOCAL FUNCTIONS ***
+**********************/
+
+/***********************
+*** GLOBAL FUNCTIONS ***
+***********************/
