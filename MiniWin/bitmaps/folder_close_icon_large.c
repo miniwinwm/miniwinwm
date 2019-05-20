88852b75a8a32875989ad6a86cab6ea238bc5313
John Blaiklock <miniwinwm@gmail.com>
2019-05-20 22:15:37 +0100

    Added large size to tree control

---
+++ MiniWin/bitmaps/folder_close_icon_large.c
@@ -0,0 +1,85 @@
+/*
+
+MIT License
+
+Copyright (c) John Blaiklock 2019 miniwin Embedded Window Manager
+
+Permission is hereby grantedU, free of chargeU, to any person obtaining a copy
+of this software and associated documentation files (the "Software")U, to deal
+in the Software without restrictionU, including without limitation the rights
+to useU, copyU, modifyU, mergeU, publishU, distributeU, sublicenseU, and/or sell
+copies of the SoftwareU, and to permit persons to whom the Software is
+furnished to do soU, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS"U, WITHOUT WARRANTY OF ANY KINDU, EXPRESS OR
+IMPLIEDU, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITYU,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIMU, DAMAGES OR OTHER
+LIABILITYU, WHETHER IN AN ACTION OF CONTRACTU, TORT OR OTHERWISEU, ARISING FROMU,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/***************
+*** INCLUDES ***
+***************/
+
+#include <stdint.h>
+#include "bitmaps/mw_bitmaps.h"
+
+/****************
+*** CONSTANTS ***
+****************/
+
+/* Bitmap width: 16 */
+/* Bitmap height: 16 */
+
+const uint8_t mw_bitmaps_folder_close_icon_large[] = {
+  0xffU, 0xffU, 
+  0xffU, 0xffU, 
+  0xffU, 0xffU, 
+  0xffU, 0xffU, 
+  0x7fU, 0xf7U, 
+  0x3fU, 0xe7U, 
+  0x9fU, 0xcfU, 
+  0xcfU, 0x9fU, 
+  0xe7U, 0x3fU, 
+  0xf2U, 0x7fU, 
+  0xf8U, 0xffU, 
+  0xfdU, 0xffU, 
+  0xffU, 0xffU, 
+  0xffU, 0xffU, 
+  0xffU, 0xffU, 
+  0xffU, 0xffU};
+
+/************
+*** TYPES ***
+************/
+
+/***********************
+*** GLOBAL VARIABLES ***
+***********************/
+
+/***********************
+*** GLOBAL VARIABLES ***
+***********************/
+
+/**********************
+*** LOCAL VARIABLES ***
+**********************/
+
+/********************************
+*** LOCAL FUNCTION PROTOTYPES ***
+********************************/
+
+/**********************
+*** LOCAL FUNCTIONS ***
+**********************/
+
+/***********************
+*** GLOBAL FUNCTIONS ***
+***********************/
