1213714c4096d113170ee78be8ae4a400634ae9d
John Blaiklock <miniwinwm@gmail.com>
2020-06-06 14:32:55 +0100

    Added camera example for windows eclipse project files

--- MiniWinCamera_Common/miniwin_user.h
+++ MiniWinCamera_Common/miniwin_user.h
@@ -1,3 +1,29 @@
+/*
+
+MIT License
+
+Copyright (c) John Blaiklock 2019 miniwin Embedded Window Manager
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #ifndef MINIWIN_USER_H
 #define MINIWIN_USER_H
 
@@ -5,8 +31,27 @@
  extern "C" {
 #endif
 
-/* extern references for window window_camera */
+/***************
+*** INCLUDES ***
+***************/
+
+/****************
+*** CONSTANTS ***
+****************/
+
+/************
+*** TYPES ***
+************/
+
+/*************************
+*** EXTERNAL VARIABLES ***
+*************************/
+
 extern mw_handle_t window_window_camera_handle;
+
+/***************************
+*** FUNCTIONS PROTOTYPES ***
+***************************/
 
 #ifdef __cplusplus
 }
