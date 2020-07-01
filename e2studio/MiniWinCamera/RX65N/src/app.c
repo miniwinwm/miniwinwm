1e7d2580e34c75f994a8e240ab4712a14ac5077f
John Blaiklock <miniwinwm@gmail.com>
2020-07-01 14:20:31 +0100

    Added rx65m camera example

---
+++ e2studio/MiniWinCamera/RX65N/src/app.c
@@ -0,0 +1,81 @@
+/*
+
+MIT License
+
+Copyright (c) John Blaiklock 2020 miniwin Embedded Window Manager
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/***************
+*** INCLUDES ***
+***************/
+
+#include "miniwin.h"
+#include "app.h"
+#include "r_gpio_rx_if.h"
+#include "camlib.h"
+
+/****************
+*** CONSTANTS ***
+****************/
+
+/************
+*** TYPES ***
+************/
+
+/***********************
+*** GLOBAL VARIABLES ***
+***********************/
+
+/**********************
+*** LOCAL VARIABLES ***
+**********************/
+
+/********************************
+*** LOCAL FUNCTION PROTOTYPES ***
+********************************/
+
+/**********************
+*** LOCAL FUNCTIONS ***
+**********************/
+
+/***********************
+*** GLOBAL FUNCTIONS ***
+***********************/
+
+void app_init(void)
+{
+	// gpio led output
+	(void)R_GPIO_PinControl(GPIO_PORT_7_PIN_0, GPIO_CMD_ASSIGN_TO_GPIO);
+	R_GPIO_PinDirectionSet(GPIO_PORT_7_PIN_0, GPIO_DIRECTION_OUTPUT);
+	(void)R_GPIO_PinControl(GPIO_PORT_7_PIN_0, GPIO_CMD_OUT_CMOS);
+
+	// gpio button input
+	(void)R_GPIO_PinControl(GPIO_PORT_0_PIN_5, GPIO_CMD_ASSIGN_TO_GPIO);
+	R_GPIO_PinDirectionSet(GPIO_PORT_0_PIN_5, GPIO_DIRECTION_INPUT);
+	(void)R_GPIO_PinControl(GPIO_PORT_0_PIN_5, GPIO_CMD_IN_PULL_UP_DISABLE);
+
+	camlib_init();
+}
+
+void app_main_loop_process(void)
+{
+}
